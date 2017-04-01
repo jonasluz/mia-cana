aef85428273c23422794e7f177124081e174efe2
Jonas de A. Luz Jr. (from notebook) <contact@jonasluz.com>
2017-04-01 20:56:42 -0300

    [FIX] Algoritmo de ordenação por inserção.

--- Playground/insertion_sort.py
+++ Playground/insertion_sort.py
@@ -6,22 +6,23 @@ Created on Sat Mar 18 17:32:54 2017
 Implementação do algoritmo de ordenação por inserção.
 """
 
-def insertionSort(values: list) -> list:
+def insertionSort(values: list, reverse=False) -> list:
     """ 
     Ordenação por inserção, modo tradicional.
     """
     
     k = None
-    for j in range(1, len(values)-1):
+    for j in range(1, len(values)):
         k = values[j]
-        i = j-1            
-        while i >= 0 and values[i] > k:
+        i = j-1
+        while i >= 0 and (values[i] < k if reverse else values[i] > k):
             values[i+1] = values[i]
             i-=1
         values[i+1] = k
                 
     return values
 
-
 # Teste 
-#print(insertionSort([7, 8, 6, 2, 5, 4, 1, 2]))
+test = [7, 8, 6, 2, 5, 4, 1, 2]
+print(insertionSort(test))
+print(insertionSort(test, True))
