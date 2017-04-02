ef8629af0289dbbccedb25fc03a9f18a3f9118f2
Jonas de A. Luz Jr. (from notebook) <contact@jonasluz.com>
2017-04-02 07:01:47 -0300

    [UPDATE] Algoritmo de ordenação por intercalação (//merge sort//). Melhoria da legibilidade e padronização do código.

--- Playground/merge_sort.py
+++ Playground/merge_sort.py
@@ -10,68 +10,63 @@ Implementação do algoritmo de ordenação por intercalação.
 import sys
 import numpy as np
 
+INF = float('Inf')
+
 def merge(a: list, p, q, r, verbose):
     """ 
     Faz a intercalação ordenada dos vetores.
     """
-    # Determina os vetores parciais
+    global INF
+    
+    ## Separa os os vetores parciais
+    #
     left, right = a[p:q+1], a[q+1:r+1]
     if verbose:
         print('Fazendo a intercalação dos vetores {} e {}'.
               format(left, right))
-        
-    result, i, j = [], 0, 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
+    left.append(INF)    # Adiciona marcador infinito ao final dos vetores
+    right.append(INF)   # 
+    
+    ## Faz a intercalação ordenada
+    #
+    i = j = 0
+    for idx in range(p, r+1):
+        if (left[i] < right[j]):
+            a[idx] = left[i]
             i+=1
-        else:
-            result.append(right[j])
+        else: 
+            a[idx] = right[j]
             j+=1
-    while i < len(left):
-        result.append(left[i])
-        i+=1
-    while j < len(right):
-        result.append(right[j])
-        j+=1
 
     if verbose:
-        print('Resultado da intercalação: {}'.format(result))
+        print('Resultado da intercalação: {}'.format(a[p:r+1]))
     
-    # Substitui valores no vetor original pelos ordenados.
-    assert r-p+1 == len(result)
-    for i in range(0,len(result)):
-        a[p+i] = result[i]
-
 def mergeSort(values: list, p=None, r=None, verbose=False):
     """
     Rotina recursiva da técnica de intercalação.
     """
+    
+    ## Atualiza valores default para os limites da ordenação.
+    #
     if p == None:                   # sem limites da lista - pegar completa.                 
         assert r == None
         p, r = 0, len(values)-1     # marca os limites da lista completa.    
     if verbose:
         print('Ordenando a lista {}'.format(values[p:r+1]))
 
-    if p == r:                      # condição de parada da recursão.
+    ## Chamadas recursivas da ordenação.
+    #
+    if p < r:
+        q = int((p+r)/2)            # calcula ponto intermediário da lista.
         if verbose:
-            print('Condição de parada atingida para ordenação de {}'.
-                  format([values[p]]))
-        return()
-    else:
-        q = p+int((r-p)/2)          # calcula ponto intermediário da lista.
-        if q < 0: sys.exit(-1)
-        if verbose:
-            print('Ponto intermediário entre {} e {} calculado: {}'.
-                  format(p, r, q))
-        # Ordena lado esquerdo.
-        mergeSort(values, p, q, verbose)
-        #Ordena lado direito.
-        mergeSort(values, q+1, r, verbose)
-        # faz a intercalação
-        merge(values, p, q, r, verbose)
-        return()
-    
+           print('Ponto intermediário entre {} e {} calculado: {}'.
+                 format(p, r, q)) 
+        mergeSort(values, p, q, verbose)    # ordena lado esquerdo.
+        mergeSort(values, q+1, r, verbose)  # ordena lado direito.
+        merge(values, p, q, r, verbose)     # faz a intercalação.
+
+    return(values)
+
 """
 TESTES
 """
@@ -83,5 +78,5 @@ print('Lista a ordenar: {}\n'.format(data))
 size = len(data)
 #sys.setrecursionlimit(1500)
 
-mergeSort(data, verbose=True)
+mergeSort(data)#, verbose=True)
 print(data)
