2d3b595c669f12708d211e84c140216a8b597a44
Jonas de A. Luz Jr. (from notebook) <contact@jonasluz.com>
2017-04-02 08:13:01 -0300

    Algoritmo de ordenação por inserção - versão recursiva.

--- Playground/insertion_sort.py
+++ Playground/insertion_sort.py
@@ -6,6 +6,8 @@ Created on Sat Mar 18 17:32:54 2017
 Implementação do algoritmo de ordenação por inserção.
 """
 
+import random 
+
 def insertionSort(values: list, reverse=False) -> list:
     """ 
     Ordenação por inserção, modo tradicional.
@@ -20,9 +22,34 @@ def insertionSort(values: list, reverse=False) -> list:
             i-=1
         values[i+1] = k
                 
-    return values
+    return(values)
 
-# Teste 
-test = [7, 8, 6, 2, 5, 4, 1, 2]
-print(insertionSort(test))
-print(insertionSort(test, True))
+def insertionSort2(values:list, i=None) -> list:
+    """
+    Ordenação por inserção, recursiva.
+    """
+    ## Ajusta valor default de i.
+    # 
+    if i == None : i = len(values)-1
+    
+    ## Rotina de ordenação por inserção.
+    # 
+    if i > 0:
+        insertionSort2(values, i-1)
+        k = values[i] ; i-=1
+        while i >= 0 and values[i] > k:
+            values[i+1] = values[i]
+            i-=1
+        values[i+1] = k
+
+    return(values)
+    
+## TESTES
+#
+data = random.sample(range(100), 13)
+print('Sequencia a ordenar:\n{}\n'.format(data))
+
+d1 = insertionSort(data.copy())
+print('Sequencia ordenada por inserção:\n{}\n'.format(d1))
+d2 = insertionSort2(data.copy())
+print('Sequencia ordenada por inserção recursiva:\n{}\n'.format(d2))
